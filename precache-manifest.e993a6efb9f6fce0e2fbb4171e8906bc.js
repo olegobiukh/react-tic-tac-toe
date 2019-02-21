0080223c171ef5518077399c86c7596b2d251f02
obiukh19 <oleg.obiukh@gmail.com>
2019-02-21 20:11:37 +0200

    Updates

--- precache-manifest.e993a6efb9f6fce0e2fbb4171e8906bc.js
+++ precache-manifest.e993a6efb9f6fce0e2fbb4171e8906bc.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/react-tic-tac-toe/static/js/runtime~main.7d1b9009.js"
   },
   {
-    "revision": "8e02e3ad30fd13c15b38",
-    "url": "/react-tic-tac-toe/static/js/main.8e02e3ad.chunk.js"
+    "revision": "7541e1619c5c1348e310",
+    "url": "/react-tic-tac-toe/static/js/main.7541e161.chunk.js"
   },
   {
     "revision": "1fcdf81b471e8996cccf",
     "url": "/react-tic-tac-toe/static/js/2.1fcdf81b.chunk.js"
   },
   {
-    "revision": "8e02e3ad30fd13c15b38",
+    "revision": "7541e1619c5c1348e310",
     "url": "/react-tic-tac-toe/static/css/main.2c58968a.chunk.css"
   },
   {
-    "revision": "928bd810ff0b25fc159a38f280aa3a1c",
+    "revision": "589d6d082f18a40113cf284dc75fbf39",
     "url": "/react-tic-tac-toe/index.html"
   }
 ];
