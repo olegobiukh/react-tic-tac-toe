7833a4728f1837dabf947ac68ab72daa141afc10
obiukh19 <oleg.obiukh@gmail.com>
2019-02-21 20:14:13 +0200

    moved styles from tags to classes

--- src/Cell.js
+++ src/Cell.js
@@ -1,23 +1,26 @@
 import React from 'react';
 
 export default function Cell(props) {
-    const colors = {
-        1: 'red',
-        2: 'yellow',
-        3: 'white'
-    }
+    // const colors = {
+    //     1: 'red',
+    //     2: 'yellow',
+    //     3: 'white'
+    // }
 
-    const stylesColor = {
-        backgroundColor: colors[props.value],
-    };
+    // const stylesColor = {
+    //     backgroundColor: colors[props.value],
+    // };
 
     return (
-        <div style={stylesColor} 
-            className="Cell" 
-            order={props.index} 
-            state={props.value} 
-            onClick={props.handleClick} 
-        />
+        <div className={(props.value === 1) ? "Cell bg_red"
+                        : (props.value === 2) ? "Cell bg_yellow"
+                        : (props.value === 3) ? "Cell bg_white"
+                        : "Cell bg_white"
+                    }
+            order={props.index}
+            state={props.value}
+            onClick={props.handleClick}
+        /> 
     )
 }
 
