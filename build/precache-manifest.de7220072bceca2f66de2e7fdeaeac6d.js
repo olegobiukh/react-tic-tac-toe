c6323b03fc44915b5ec5b770fdb30c404325ab8f
obiukh19 <oleg.obiukh@gmail.com>
2019-02-21 08:29:19 +0200

    Deplay 1

---
+++ build/precache-manifest.de7220072bceca2f66de2e7fdeaeac6d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-tic-tac-toe/build/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "dc2075735ba7c50601cb",
+    "url": "/react-tic-tac-toe/build/static/js/runtime~main.dc207573.js"
+  },
+  {
+    "revision": "626440708674f674bec4",
+    "url": "/react-tic-tac-toe/build/static/js/main.62644070.chunk.js"
+  },
+  {
+    "revision": "ba1ca5ba3f9c1e3c91ad",
+    "url": "/react-tic-tac-toe/build/static/js/2.ba1ca5ba.chunk.js"
+  },
+  {
+    "revision": "626440708674f674bec4",
+    "url": "/react-tic-tac-toe/build/static/css/main.f34bb286.chunk.css"
+  },
+  {
+    "revision": "edbe4d769ed53e395450ffbd84835dd6",
+    "url": "/react-tic-tac-toe/build/index.html"
+  }
+];
